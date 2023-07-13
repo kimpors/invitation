c34f563ac4b7d169ca22d01813f9d9306366c085
kimpors <mikita_korshak@ukr.net>
2023-07-13 07:37:22 +0300

    Add Invite Model

---
+++ Invitation/Models/Invite.cs
@@ -0,0 +1,10 @@
+namespace Invitation.Models;
+
+public class Invite
+{
+  public string Name { get; set; }
+  public string LastName { get; set; }
+  public string Phone { get; set; }
+  public string Email { get; set; }
+  public bool IsComeUp { get; set; }
+}
